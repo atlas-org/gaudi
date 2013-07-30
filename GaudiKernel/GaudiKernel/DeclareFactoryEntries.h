d72a1b42cc26f479673b073ec5b952c71249035d
Marco Clemencic <marco.clemencic@cern.ch>
2013-07-30 17:06:51 +0000

    restored old dummy macros "DECLARE_*", still used in ATLAS
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/branches/GAUDI/GAUDI_ATLAS_no_reflex@7266 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiKernel/GaudiKernel/DeclareFactoryEntries.h
+++ GaudiKernel/GaudiKernel/DeclareFactoryEntries.h
@@ -3,6 +3,40 @@
 
 #ifdef GAUDI_NEW_PLUGIN_SERVICE
 #warning "obsolete empty header, please remove it"
+#else
+
+#define DECLARE_ALGORITHM(x)  /*dummy*/
+#define DECLARE_NAMESPACE_ALGORITHM(n,x) /*dummy*/
+
+#define DECLARE_AUDITOR(x)  /*dummy*/
+#define DECLARE_NAMESPACE_AUDITOR(n,x)  /*dummy*/
+
+#define DECLARE_CONVERTER(x) /*dummy*/
+#define DECLARE_NAMESPACE_CONVERTER(n,x)   /*dummy */
+
+#define DECLARE_OBJECT(x)       /* dummy */
+#define DECLARE_DATAOBJECT(x)   /* dummy */
+#define DECLARE_NAMESPACE_OBJECT(n,x)      /* dummy */
+#define DECLARE_NAMESPACE_DATAOBJECT(n,x)  /* dummy */
+
+#define DECLARE_SERVICE(x)  /*dummy*/
+#define DECLARE_NAMESPACE_SERVICE(n,x) /*dummy*/
+
+#define DECLARE_ALGTOOL(x)             /*dummy*/
+#define DECLARE_NAMESPACE_ALGTOOL(n,x) /*dummy*/
+#define DECLARE_TOOL(x)                /*dummy*/
+#define DECLARE_NAMESPACE_TOOL(n,x)    /*dummy*/
+
+
+#define DECLARE_FACTORY_ENTRIES(x) \
+void x##_load(); \
+\
+extern "C" void x##_loadRef() { \
+  x##_load();  \
+} \
+\
+void x##_load()
+
 #endif
 
 #endif // GAUDIKERNEL_DECLAREFACTORYENTRIES_H
